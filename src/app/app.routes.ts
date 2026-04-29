5f06a3bf88af140640fc67d3f28504ad1fb1b026
Chris <christopherwdehaan@gmail.com>
2026-04-29 15:15:23 -0400

    add basic routing and home/error component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('@app/components/home/home').then((m) => m.Home)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('@app/components/error/error').then((m) => m.Error)
+  },
+];

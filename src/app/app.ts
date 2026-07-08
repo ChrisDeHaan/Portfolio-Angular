6e8bf03464767d64088d260f78a3fba84db8d7b6
Chris <christopherwdehaan@gmail.com>
2026-07-08 10:04:10 -0400

    make base template for future projects

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
 import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, TranslocoPipe],
+  imports: [RouterOutlet, TranslocoPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

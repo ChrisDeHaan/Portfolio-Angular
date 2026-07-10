7b2adeb122956b1667c598cebd4427de8833b8f2
Chris <christopherwdehaan@gmail.com>
2026-07-10 11:48:58 -0400

    style mat tabs

---
+++ src/app/components/accordion-group/accordion-group.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion-group',
+  imports: [],
+  templateUrl: './accordion-group.component.html',
+  styleUrl: './accordion-group.component.scss'
+})
+export class AccordionGroupComponent {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,15 @@ import { BaseComponent } from '@app/abstracts/base.component';
 import { HeaderComponent } from '../header/header.component';
 import { TabGroupComponent } from '../tab-group/tab-group.component';
 import { FooterComponent } from '../footer/footer.component';
+import { AccordionGroupComponent } from '../accordion-group/accordion-group.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HeaderComponent,
     TabGroupComponent,
-    FooterComponent
+    FooterComponent,
+    AccordionGroupComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'

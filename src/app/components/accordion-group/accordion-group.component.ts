077bc9dfad1f6fd1e75e76e3ac9b2ab01385ae1a
Chris <christopherwdehaan@gmail.com>
2026-07-11 16:47:56 -0400

    add expansion panel for mobile view

--- src/app/components/accordion-group/accordion-group.component.ts
+++ src/app/components/accordion-group/accordion-group.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BaseComponent } from '@app/abstracts/base.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { TranslocoPipe } from '@jsverse/transloco';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-accordion-group',
-  imports: [],
+  imports: [
+    MatExpansionModule,
+    AboutMeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    TranslocoPipe,
+    MatIcon
+],
   templateUrl: './accordion-group.component.html',
   styleUrl: './accordion-group.component.scss'
 })
-export class AccordionGroupComponent {}
+export class AccordionGroupComponent extends BaseComponent {}
